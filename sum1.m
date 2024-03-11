cc493629b35ae24914bd03214ea1da384600c6c3
denisatrofin <92776233+denisatrofin@users.noreply.github.com>
2024-03-11 21:26:00 +0200

    Add files via upload

---
+++ sum1.m
@@ -0,0 +1,17 @@
+
+close all;
+F1=80;
+F2=100;
+N=3;
+A1=5;
+A2=6; 
+Vo1=12;
+Vo2=-12;
+tip=1;
+tip1=1;
+R1=5;
+R2=5.5;
+R=10;
+DC1=0;
+DC2=0;
+sumator1(tip,tip1,R,R1,R2,A1,A2,F1,F2,N,Vo1,Vo2, DC1, DC2)
---
+++ sum2.m
@@ -0,0 +1,16 @@
+
+close all;
+F1=50;
+F2=50;
+N=3;
+A1=5;
+A2=6;
+Vo1=12;
+Vo2=-12;
+tip=1;
+tip1=1;
+R1=5.5;
+R2=10;
+R3=2.2;
+R4=5;
+sumator2(tip,tip1,R1,R2,R3,R4,A1,A2,F1,F2,N,Vo1,Vo2);   
